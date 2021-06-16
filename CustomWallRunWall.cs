eab87d13b46d88fca4d0798640400f7605df7e79
Coder <60108915+NVcoder24@users.noreply.github.com>
2021-06-16 15:30:00 +0400

    Create CustomWallRunWall.cs

---
+++ CustomWallRunWall.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomWallRunWall : MonoBehaviour
+{
+    public Vector3 wallRunJump = new Vector3(15f, 7f, 15f);
+    public float wallRunJumpMul = 50f;
+    public float wallRunGravity = 0.5f;
+}
