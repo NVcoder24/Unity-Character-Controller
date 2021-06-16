c7b1fbc67ad1e6018ff6d529a024adb41044f7e7
Coder <60108915+NVcoder24@users.noreply.github.com>
2021-06-16 15:29:17 +0400

    Update PlayerCon.cs

--- PlayerCon.cs
+++ PlayerCon.cs
@@ -11,6 +11,7 @@
 
 // Libraries
 using UnityEngine;
+using TMPro;
 
 // Main script`s class "PlayerCon" (mono behaviour)
 public class PlayerCon : MonoBehaviour
@@ -22,8 +23,8 @@ public class PlayerCon : MonoBehaviour
     [SerializeField] float playerSpeedRun = 12f;
     [SerializeField] float speedMul = 10f;
     [SerializeField] float speedMulAir = 2f;
-    [SerializeField] float jumpForceWallRun = 10f;
-    [SerializeField] Vector3 jumpForce = new Vector3(0, 10, 0);
+    [SerializeField] Vector3 jumpForce = new Vector3(0f, 10f, 0f);
+    [SerializeField] Vector3 jumpWallRun = new Vector3(15f, 7f, 15f);
 
     [Header("Camera")]
     // Camera stuff
@@ -49,14 +50,21 @@ public class PlayerCon : MonoBehaviour
     [SerializeField] KeyCode jumpKey = KeyCode.Space;
     [SerializeField] KeyCode runKey = KeyCode.LeftShift;
 
+    [Header("Sounds")]
+    // Sound stuff
+
     // Rigidbody
     Rigidbody rb = null;
 
-    // Start
-    void Start() {
-        // Rigidbody setup
+    // Rigidbody setup
+    void setupRB() {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+    }
+
+    // Start
+    void Start() {
+        setupRB();
     }
 
     // Predefining variables
@@ -71,16 +79,17 @@ public class PlayerCon : MonoBehaviour
     RaycastHit leftWallHit;                    // Wall left  raycast hit
     RaycastHit rightWallHit;                   // Wall right raycast hit
 
-    // Update loop
-    void Update() {
-        // Rigidbody drag control
+    // Rigidbody drag control
+    void dragCon() {
         if (isGrounded) {
             rb.drag = drag;    // Rigidbody drag
         } else {
             rb.drag = dragAir; // In air Rigidbody drag
         }
+    }
 
-        // Camera rotation
+    // Camera rotation
+    void cameraRotation() {
         float camX = Input.GetAxis("Mouse X"); // Mouse X
         float camY = Input.GetAxis("Mouse Y"); // Mouse Y
 
@@ -91,23 +100,29 @@ public class PlayerCon : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(0, xRotation, 0);                // Camera rotation
         Camera.transform.rotation = Quaternion.Euler(yRotation, xRotation, tilt); // Player rotation
+    }
 
-        // Jumping
+    // Jumping
+    void jumping() {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, transform.localScale.y + 0.2f); // Is grounded (raycast)
 
         if (isGrounded && Input.GetKeyDown(jumpKey)) {
             rb.AddForce(jumpForce, ForceMode.Impulse); // Jumping (impulse)
         }
+    }
 
-        // Cursor state control
+    // Cursor state control
+    void cursorState() {
         if (isLocked) {
             Cursor.lockState = CursorLockMode.Locked;   // Lock cursor
         } else {
             Cursor.lockState = CursorLockMode.Confined; // Unlock cursor
         }
         Cursor.visible = !isLocked; // Cursor visibility
+    }
 
-        // Slope detection
+    // Slope detection
+    void slopeHandling() {
         RaycastHit slopeHit; // Raycast hit info
         if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, transform.localScale.y + 0.5f)) { // If something under player
             if (slopeHit.normal != Vector3.up) { // If slope not straight
@@ -120,66 +135,98 @@ public class PlayerCon : MonoBehaviour
         }
 
         slopeMoveDirection = Vector3.ProjectOnPlane(direction, slopeHit.normal); // Set slope move direction (project on plane)
+    }
 
-        // Wall run
-        wallLeft = Physics.Raycast(transform.position, -transform.right, out leftWallHit, 1f);
-        wallRight = Physics.Raycast(transform.position, transform.right, out rightWallHit, 1f);
+    // Wall run
+    void wallRunning() {
+        wallLeft = Physics.Raycast(transform.position, -transform.right, out leftWallHit, 1f);  // Raycasting for left  wall
+        wallRight = Physics.Raycast(transform.position, transform.right, out rightWallHit, 1f); // Raycasting for right wall
 
-        if (!Physics.Raycast(transform.position, Vector3.down, transform.localScale.y + 0.2f)) {
+        if (!Physics.Raycast(transform.position, Vector3.down, transform.localScale.y + 0.2f)) { // If player in air
             if (wallLeft) {
-                StartWR();
+                StartWR(); // Start wall run on left  wall
             } else if (wallRight) {
-                StartWR();
+                StartWR(); // Start wall run on right wall
             } else {
-                StopWR();
+                StopWR();  // Stop wall run
             }
         } else {
-            StopWR();
+            StopWR();      // Stop wall run
         }
+    }
+
+    // Update loop
+    void Update() {
+        dragCon();
+        cameraRotation();
+        jumping();
+        cursorState();
+        slopeHandling();
+        wallRunning();
     }
 
     // Start wall run
     void StartWR() {
-        rb.useGravity = false;
+        rb.useGravity = false; // Turn Rigidbody gravity
 
-        rb.AddForce(Vector3.down * wallRunGravity, ForceMode.Force);
-
-        Camera.fieldOfView = Mathf.Lerp(Camera.fieldOfView, wallRunFov, fovChangeTime * Time.deltaTime);
+        Camera.fieldOfView = Mathf.Lerp(Camera.fieldOfView, wallRunFov, fovChangeTime * Time.deltaTime); // Change field of view for effect
 
         if (wallLeft)
-            tilt = Mathf.Lerp(tilt, -wallRunTilt, tiltChangeTime * Time.deltaTime);
+            tilt = Mathf.Lerp(tilt, -wallRunTilt, tiltChangeTime * Time.deltaTime); // Camera tilt for left  wall for effect
         else if (wallRight)
-            tilt = Mathf.Lerp(tilt, wallRunTilt, tiltChangeTime * Time.deltaTime);
+            tilt = Mathf.Lerp(tilt, wallRunTilt, tiltChangeTime * Time.deltaTime);  // Camera tilt for right wall for effect
 
+        // Wall run settings control
+        Vector3 wallRunJump = jumpWallRun; // Static wall run jump
+        float mul = wallRunJumpMul;        // Static wall run jump multiplier
+        float gravity = wallRunGravity;    // Static wall run gravity
+
+        if (wallLeft && leftWallHit.collider.gameObject.GetComponent<CustomWallRunWall>() != null) {
+            CustomWallRunWall wall = leftWallHit.collider.gameObject.GetComponent<CustomWallRunWall>(); // Custom wall
+            wallRunJump = wall.wallRunJump; // Set wall run jump
+            mul = wall.wallRunJumpMul;      // Set wall run jump multiplier
+            gravity = wall.wallRunGravity;  // Set wall run gravity
+        }
+
+        if (wallRight && rightWallHit.collider.gameObject.GetComponent<CustomWallRunWall>() != null) {
+            CustomWallRunWall wall = rightWallHit.collider.gameObject.GetComponent<CustomWallRunWall>(); // Custom wall
+            wallRunJump = wall.wallRunJump; // Set wall run jump
+            mul = wall.wallRunJumpMul;      // Set wall run jump multiplier
+            gravity = wall.wallRunGravity;  // Set wall run gravity
+        }
+
+        // Wall run gravity
+        rb.AddForce(Vector3.down * gravity, ForceMode.Force); // Add wall run gravity force
+
+        // Jumping
         if (Input.GetKeyDown(jumpKey))
         {
             if (wallLeft)
             {
-                Vector3 wallRunJumpDirection = transform.up + leftWallHit.normal;
-                rb.AddForce(wallRunJumpDirection * jumpForceWallRun * wallRunJumpMul, ForceMode.Force);
+                Vector3 wallRunJumpDirection = transform.up + leftWallHit.normal; // Get wall direction
+                rb.AddForce(new Vector3(wallRunJump.x * wallRunJumpDirection.x, wallRunJump.y, wallRunJump.z * wallRunJumpDirection.z) * mul); // Add force
             }
             else if (wallRight)
             {
-                Vector3 wallRunJumpDirection = transform.up + rightWallHit.normal;
-                rb.AddForce(wallRunJumpDirection * jumpForceWallRun * wallRunJumpMul, ForceMode.Force);
+                Vector3 wallRunJumpDirection = transform.up + rightWallHit.normal; // Get wall direction
+                rb.AddForce(new Vector3(wallRunJump.x * wallRunJumpDirection.x, wallRunJump.y, wallRunJump.z * wallRunJumpDirection.z) * mul); // Add force
             }
         }
     }
 
     // Stop wall run
     void StopWR() {
-        rb.useGravity = true;
+        rb.useGravity = true; // Turn Rigidbody gravity on
 
-        Camera.fieldOfView = Mathf.Lerp(Camera.fieldOfView, staticFov, fovChangeTime * Time.deltaTime);
-        tilt = Mathf.Lerp(tilt, staticTilt, tiltChangeTime * Time.deltaTime);
+        Camera.fieldOfView = Mathf.Lerp(Camera.fieldOfView, staticFov, fovChangeTime * Time.deltaTime); // Reset camera field of view
+        tilt = Mathf.Lerp(tilt, staticTilt, tiltChangeTime * Time.deltaTime);                           // Reset camera tilt
     }
 
     // Predefining variables
     Vector3 direction = Vector3.zero;
 
-    // Update loop (fixed) for physics stuff
-    void FixedUpdate() {
-        // Player Movement
+    // Player Movement
+    void movement() {
         float movY = Input.GetAxisRaw("Horizontal"); // X axis
         float movX = Input.GetAxisRaw("Vertical");   // Y axis
 
@@ -187,6 +234,7 @@ public class PlayerCon : MonoBehaviour
 
         direction = transform.forward * movX + transform.right * movY; // Force direction
 
+        // Speed manager
         float mul = speedMul;  // Speed multiplier
         if (!isGrounded) {
             mul = speedMulAir; // In air speed multiplier
@@ -203,5 +251,10 @@ public class PlayerCon : MonoBehaviour
         } else {
             rb.AddForce(slopeMoveDirection.normalized * speed * mul, ForceMode.Acceleration); // Add force to Rigidbody on slope (acceleration)
         }
+    }
+
+    // Update loop (fixed) for physics stuff
+    void FixedUpdate() {
+        movement();
     }
 }
